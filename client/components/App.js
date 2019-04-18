236450e6308eca2e501d0ac96eb4ce5cb118d895
Raichung <cliff.ahleong@gmail.com>
2019-04-19 09:57:23 +1200

    Bring in API

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,37 @@
 import React from 'react'
+// import * as api from '../api'
+import {getSatellite} from "../api"
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    this.printSatellite = this.printSatellite.bind(this);
+  }
+  componentDidMount(){
+    getSatellite(this.printSatellite)
+  }
+  
+  printSatellite(err, data) {
+    this.setState({
+      name: data.name,
+      latitude: data.latitude,
+      longitude: data.longitude,
+      altitude: data.altitude,
+      velocity: data.velocity
+    })
+    console.log(this.state)
+  }
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+  render() {
+    return (
+      <div>
+        <h2>{this.state.name}</h2>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
 
